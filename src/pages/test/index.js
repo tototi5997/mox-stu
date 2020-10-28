7181a180d5671eaca1481bbf59981efacf1a8d05
zhangqian5997 <873440829@qq.com>
2020-10-28 16:48:47 +0800

    first-commit

---
+++ src/pages/test/index.js
@@ -0,0 +1,14 @@
+// import Button from 'antd/lib/button/button'
+import React from 'react'
+import {Button} from 'antd'
+
+export default class Test extends React.Component{
+    render(){
+        return(
+            <div>
+                这是一段测试文字
+                <Button>测试按钮</Button>
+            </div>
+        );
+    }
+}
